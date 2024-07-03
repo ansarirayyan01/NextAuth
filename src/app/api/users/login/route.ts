98394ddd13da5e59dc76ad5ce1731ddd096049ae
ansarirayyan01 <ansarirayyan874@gmail.com>
2024-07-03 22:12:06 +0530

    added backend of login route for user

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,51 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextResponse, NextRequest } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { message: "User does not exists" },
+        { status: 400 }
+      );
+    }
+
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json(
+        { message: "Please check your credentials" },
+        { status: 400 }
+      );
+    }
+
+    const tokenData = {
+      userId: user._id,
+      email: user.email,
+      username: user.username,
+    };
+
+    const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+    const response = NextResponse.json({
+      message: "Login successful",
+      success: true,
+    });
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+    return response;
+    
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
